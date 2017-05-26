6a583bc67b5aa37662d35784858cdbd0d50ad3dc
Anatoliy Vershinin <cata07@yandex.ru>
2017-05-26 16:04:33 +0300

    add header-top

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,22 @@ document.addEventListener('DOMContentLoaded', function dom() {
     }
     nojsreplace();
 
+    selectForm();
+    function selectForm() {
+      $(".select").on("click",".select__title", function () {
+        var parent = $(this).closest(".select");
+        if (!parent.hasClass("is-open")) {
+          parent.addClass("is-open");
+          $(".select.is-open").not(parent).removeClass("is-open");
+        } else {
+          parent.removeClass("is-open");
+        }
+      }).on("click","ul>li",function() {
+        var parent = $(this).closest(".select");
+        parent.removeClass("is-open").find(".select__title").text($(this).text());
+        parent.find("input[type=hidden]").attr("value", $(this).attr("data-value"));
+      });
+    }
 
     // function toggleMenu() {
     //   var menuHam = $(".menu__hamburger");
--- gulp/paths/js.bundle.js
+++ gulp/paths/js.bundle.js
@@ -1,5 +1,7 @@
 
 
 module.exports = [
-  './bower_modules/picturefill/dist/picturefill.js'
+  './bower_modules/picturefill/dist/picturefill.js',
+  './bower_modules/jQuery/dist/jquery.js',
+  './bower_modules/bootstrap/dist/js/bootstrap.js',
 ];
