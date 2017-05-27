dc7e7229d93f305a4bc77008201fca02b4b52f5d
Anatoliy Vershinin <cata07@yandex.ru>
2017-05-27 16:32:44 +0300

    add header-middle, header-bottom

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,17 @@
 document.addEventListener('DOMContentLoaded', function dom() {
   (function() { //область видимости
     // js-status
-    function nojsreplace() {
-      // if (document.documentElement.html.className == "no-js") {
-      //   document.body.classList.remove("no-js");
-      // }
-      // console.log(document.documentElement);
+
+
+    toggleMenu();
+    function toggleMenu() {
+      var menuHam = $(".menu__hamburger");
+      var menu = $(".menu__list");
+      var action = $(".header__action");
+      menuHam.on("click", function(e) {
+        menuHam.toggleClass("is-active");
+      });
     }
-    nojsreplace();
 
     selectForm();
     function selectForm() {
